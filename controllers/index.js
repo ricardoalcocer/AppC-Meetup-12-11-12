aade73a9e1e09d7306e5042e2f51fab0545b054c
Ricardo Alcocer <alco@ricardoalcocer.com>
2012-12-05 15:23:49 -0800

    Initial Commit

---
+++ controllers/index.js
@@ -0,0 +1,17 @@
+function showmenu(e){
+	$.main.width=Ti.Platform.displayCaps.platformWidth;
+	$.main.animate({
+		left:300,
+		duration:100
+	});
+}
+
+function hidemenu(e){
+	$.main.width=Ti.Platform.displayCaps.platformWidth;
+	$.main.animate({
+		left:0,
+		duration:100
+	});
+}
+
+$.index.open();
