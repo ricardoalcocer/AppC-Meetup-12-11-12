453e32f94d2ff7a52c19544274f9f1da6025ffe4
Ricardo Alcocer <alco@ricardoalcocer.com>
2012-12-07 12:29:01 -0800

    Fixed layout problems when changing orientation

--- controllers/index.js
+++ controllers/index.js
@@ -1,8 +1,10 @@
+// dummy click event.  Add your own!
 function testclick(e){
-	alert('Clicked');
+	alert('Clicked ' + '\'' + e.source.id + '\'');
 }
 
 function showmenu(e){
+	// have to set the current width of the "main" view before moving it so it doesn't get sweezed
 	$.main.width=Ti.Platform.displayCaps.platformWidth;
 	$.main.animate({
 		left:300,
@@ -11,11 +13,17 @@ function showmenu(e){
 }
 
 function hidemenu(e){
+	// have to set the current width of the "main" view before moving it so it doesn't get sweezed
 	$.main.width=Ti.Platform.displayCaps.platformWidth;
 	$.main.animate({
 		left:0,
 		duration:100
 	});
 }
+
+// This bit listens to the orientation change and re-establishes the width of the "main" view, allowing the layout to survive after oriantation changes
+Ti.Gesture.addEventListener('orientationchange', function(e) {
+    $.main.width=Ti.Platform.displayCaps.platformWidth;
+});
 
 $.index.open();
