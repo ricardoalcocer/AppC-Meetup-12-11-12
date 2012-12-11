233a33ed1ab38cfc21abae15dfd14a415439a9a6
Ricardo Alcocer <alco@ricardoalcocer.com>
2012-12-10 16:09:16 -0800

    Added Like button and some performance and structural enhancements.

--- controllers/index.js
+++ controllers/index.js
@@ -3,25 +3,33 @@ function testclick(e){
 	alert('Clicked ' + '\'' + e.source.id + '\'');
 }
 
-function showmenu(e){
+// handles the "Like button"
+function likeunlike(e){
+	// change like/unlike image, based on the current image
+	if (e.source.image==="likebtn.png"){
+		e.source.image="likebtnOn.png"
+	}else{
+		e.source.image="likebtn.png"
+	}
+}
+
+// shows or hide the menu
+function showhidemenu(e){
+	var newLeft=0;
+	if ($.main.rect.left==0) newLeft=300;
+	
 	// have to set the current width of the "main" view before moving it so it doesn't get sweezed
+	// try commenting out the following line and setting the "newLeft" to 200 instead of 
+	// 300 to see what I mean
 	$.main.width=Ti.Platform.displayCaps.platformWidth;
 	$.main.animate({
-		left:300,
+		left:newLeft,
 		duration:100
 	});
 }
 
-function hidemenu(e){
-	// have to set the current width of the "main" view before moving it so it doesn't get sweezed
-	$.main.width=Ti.Platform.displayCaps.platformWidth;
-	$.main.animate({
-		left:0,
-		duration:100
-	});
-}
-
-// This bit listens to the orientation change and re-establishes the width of the "main" view, allowing the layout to survive after oriantation changes
+// This bit listens to the orientation change and re-establishes the width 
+// of the "main" view, allowing the layout to survive after oriantation changes
 Ti.Gesture.addEventListener('orientationchange', function(e) {
     $.main.width=Ti.Platform.displayCaps.platformWidth;
 });
